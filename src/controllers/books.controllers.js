0e1733dc422e518f1159ed90b911728e6071dddd
sr-roboto <marcoslopez123.lu@gmail.com>
2024-06-27 16:31:48 -0300

    corregi un problema, de que cuando se creaba un nuevo libro el year no me lo tomaba como entero

--- src/controllers/books.controllers.js
+++ src/controllers/books.controllers.js
@@ -17,18 +17,20 @@ ctrl.obtenerLibro = (req, res) => {
 ctrl.crearLibro = (req, res) => {
     const { title, author, year } = req.body;
     const id = db.length + 1;
-    db.push({ id, title, author, year })
+    const yearNumber = parseInt(year)
+    db.push({ id, title, author, yearNumber })
     res.json({ "msg": "Libro creado con exito" });
 };
 
 ctrl.editarLibro = (req, res) => {
     const id = parseInt(req.params.id)
     const { title, author, year } = req.body
+    const yearNumber = parseInt(year)
     db.forEach((user) => {
         if (user.id === id) {
             user.title = title;
             user.author = author;
-            user.year = year;
+            user.year = yearNumber;
         }
     });
     res.json({ "msg": "libro editado correctamente" });
